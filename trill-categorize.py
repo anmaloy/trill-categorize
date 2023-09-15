84aa31c31f933c78a11caac16fef5a247f748f48
Andrew Maloy <anmaloy@gmail.com>
2023-09-15 15:28:31 -0600

    Fixed drift issues and simplified

--- trill-categorize.py
+++ trill-categorize.py
@@ -50,53 +50,47 @@ def makeMemMapRaw(binFullPath, meta):
     return rawData
 
 
-def get_imec_analog(imec_bin_fn, chan_id, t0, tf):
+def get_ni_analog(ni_bin_fn, chan_id):
     '''
-    Convenience function to load in an imce analog channel
-    :param imec_bin_fn: filename to load from
+    Convenience function to load in a NI analog channel
+    :param ni_bin_fn: filename to load from
     :param chan_id: channel index to load
     :return: analog_dat
     '''
-    if type(chan_id) is int:
-        chan_id = [chan_id]
-    meta = readMeta(Path(imec_bin_fn))
-    sr = SampRate(meta)
-    s0, sf = (int(t0 * sr), int(tf * sr))
-    tvec = np.arange(t0, tf, 1 / sr)
+    meta = readMeta(Path(ni_bin_fn))
     bitvolts = Int2Volts(meta)
-    imec_dat = makeMemMapRaw(imec_bin_fn, meta)
-    analog_dat = imec_dat[chan_id, s0:sf] * bitvolts
-    tvec = tvec[:analog_dat.shape[1]]
+    ni_dat = makeMemMapRaw(ni_bin_fn, meta)
+    analog_dat = ni_dat[chan_id] * bitvolts
+    sr = SampRate(meta)
+    tvec = np.linspace(0, len(analog_dat)/sr, len(analog_dat))
+
     return tvec, analog_dat
 
 
-chan_id = [0]
-binPath = Path('data\\NPX-S2-28_g0_tcat.nidq.bin')
+chan_id = 0
+binPath = Path('data\\NPX-S2-28_g0_t1.nidq.bin')
 meta = readMeta(binPath)
 sRate = SampRate(meta)
 ftime = float(meta['fileTimeSecs'])
 
-tStart = 0
-tEnd = ftime
-
-ni_time, ni_data = get_imec_analog(binPath, chan_id, tStart, tEnd)
-ni_data = ni_data.T
-ni_data = np.squeeze(ni_data)
+ni_time, ni_data = get_ni_analog(binPath, chan_id)
 ni_time = ni_time*1000
 # height: good mV height for filtering out spikes, distance: ms between spikes converted for the x-axis here
-peaks, properties = find_peaks(ni_data, height=0.02, distance=10*sRate/1000)
-peaks_time = [int(x//(sRate/1000)) for x in peaks]
+cutoff = 0.02
+distance = 10
+peaks, properties = find_peaks(ni_data, height=cutoff, distance=distance*sRate/1000)
+peaks_time = ni_time[peaks]
 
-# plt.ylim(-0.4, 0.7)
-# plt.plot(ni_time, ni_data, linewidth=0.1)
-# plt.plot(peaks_time, ni_data[peaks], 'x')
-# plt.xlabel('Times (ms)')
-# plt.ylabel('Nerve (V)')
-# plt.title('Overlaid NIDAQ Example')
-# plt.show()
+plt.plot(ni_time, ni_data, linewidth=0.1)
+plt.plot(peaks_time, ni_data[peaks], '.')
+plt.ylim(-0.4, 0.7)
+plt.axhline(y=cutoff, color='red')
+plt.xlabel('Times (ms)')
+plt.ylabel('Nerve (V)')
+plt.title('NIDAQ Nerve Spikes')
+plt.show()
 
 df = pd.DataFrame(columns=['time(ms)', 'amplitude(v)'])
 df['time(ms)'] = peaks_time
 df['amplitude(v)'] = ni_data[peaks]
-print(df)
 df.to_csv('data\\NPX-S2-28_g0-data.csv', index=False)
