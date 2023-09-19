f649e33c166a126ed97b2ce2ae49c6db7aef8048
Andrew Maloy <anmaloy@gmail.com>
2023-09-19 15:28:48 -0600

    Add better visualization.

--- trill-categorize.py
+++ trill-categorize.py
@@ -1,7 +1,14 @@
 import numpy as np
 from pathlib import Path
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+import matplotlib.patches as mpatches
+from scipy.signal import chirp, find_peaks, peak_widths
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
 def readMeta(binFullPath):
@@ -69,30 +76,91 @@ def get_ni_analog(ni_bin_fn, chan_id):
 
 chan_id = 0
 fileName = 'NPX-S2-27'
-gate = 0
+gate = 3
 binPath = Path(f'data\\{fileName}\\catgt_{fileName}_g{gate}\\{fileName}_g{gate}_tcat.nidq.bin')
 meta = readMeta(binPath)
 sRate = SampRate(meta)
-ftime = float(meta['fileTimeSecs'])
 
 ni_time, ni_data = get_ni_analog(binPath, chan_id)
 # height: amplitude in v for filtering out spikes, distance: ms between spikes to avoid duplicates
-cutoff = 0.06
+cutoff = 0.03
 distance = 10
 peaks, properties = find_peaks(ni_data, height=cutoff, distance=distance*sRate/1000)
+width_half = peak_widths(ni_data, peaks, rel_height=0.5)
+lst = list(width_half)
+lst[0] = lst[0]/sRate
+lst[2:] = (x/sRate for x in lst[2:])
+width_half = tuple(lst)
+width_full = peak_widths(ni_data, peaks, rel_height=0.75)
+lst = list(width_full)
+lst[0] = lst[0]/sRate
+lst[2:] = (x/sRate for x in lst[2:])
+width_full = tuple(lst)
 peaks_time = ni_time[peaks]
 
-plt.plot(ni_time, ni_data, linewidth=0.1)
-plt.plot(peaks_time, ni_data[peaks], '.')
+targetdf = pd.read_csv('data\\targets.csv')
+# ~55ms was the time the original data was adjusted by
+targetdf.start = (targetdf.start+55)/1000
+targetdf.end = (targetdf.end+55)/1000
+
+columns = ['time(s)', 'amplitude(v)', 'delay(s)', 'fwidth(len)', 'fwidth(amp)', 'hwidth(len)', 'hwidth(amp)', 'type']
+df = pd.DataFrame(columns=columns)
+df['time(s)'] = peaks_time
+df['amplitude(v)'] = ni_data[peaks]
+df['delay(s)'] = df['time(s)'].diff()
+df['fwidth(len)'] = width_full[0]
+df['fwidth(amp)'] = width_full[1]
+df['hwidth(len)'] = width_half[0]
+df['hwidth(amp)'] = width_half[1]
+lst = targetdf.apply(lambda row: (row['start'], row['end']), axis=1)
+conditions = [(df['time(s)'] > x[0]) & (df['time(s)'] < x[1]) for x in lst]
+choices = targetdf.type.values
+df['type'] = np.select(conditions, choices, default=np.nan)
+df = df.dropna()
+
+plt.plot(ni_time, ni_data, linewidth=0.05)
+for index, row in df.iterrows():
+    if row[7] == 'S':
+        plt.plot(peaks_time[index], ni_data[peaks][index], '.', color='tomato', alpha=0.75)
+    elif row[7] == 'F':
+        plt.plot(peaks_time[index], ni_data[peaks][index], '.', color='forestgreen', alpha=0.75)
+    elif row[7] == 'I':
+        plt.plot(peaks_time[index], ni_data[peaks][index], '.', color='olive', alpha=0.75)
+    elif row[7] == 'VC':
+        plt.plot(peaks_time[index], ni_data[peaks][index], '.', color='slateblue', alpha=0.75)
+    elif row[7] == 'B':
+        plt.plot(peaks_time[index], ni_data[peaks][index], '.', color='silver', alpha=0.75)
+    else:
+        plt.plot(peaks_time[index], ni_data[peaks][index], '.', color='black', alpha=0.75)
+plt.hlines(*width_half[1:], color="C2")
+plt.hlines(*width_full[1:], color="C3")
 plt.ylim(-0.4, 0.7)
-plt.axhline(y=cutoff, color='red')
+plt.axhline(y=cutoff, color='red', linewidth=0.3)
+legend = [mpatches.Patch(color='yellow', label='Intro Trill', alpha=0.5),
+          mpatches.Patch(color='green', label='Fast Trill', alpha=0.5),
+          mpatches.Patch(color='red', label='Slow Trill', alpha=0.5),
+          mpatches.Patch(color='blue', label='Vocal Comp AP', alpha=0.5),
+          mpatches.Patch(color='grey', label='Breathing', alpha=0.5)]
+plt.legend(handles=legend, loc='lower right')
 plt.xlabel('Times (s)')
 plt.ylabel('Nerve (v)')
 plt.title('NIDAQ Nerve Spikes')
+lst = targetdf[['start', 'end']]
+for index, row in targetdf.iterrows():
+    if row[3] == 'S':
+        plt.axvspan(row[1], row[2], color='red', alpha=0.25)
+    if row[3] == 'F':
+        plt.axvspan(row[1], row[2], color='green', alpha=0.25)
+    if row[3] == 'I':
+        plt.axvspan(row[1], row[2], color='yellow', alpha=0.25)
+    if row[3] == 'VC':
+        plt.axvspan(row[1], row[2], color='blue', alpha=0.25)
+    if row[3] == 'B':
+        plt.axvspan(row[1], row[2], color='grey', alpha=0.25)
 plt.show()
 
-df = pd.DataFrame(columns=['time(s)', 'amplitude(v)'])
-df['time(s)'] = peaks_time
-df['amplitude(v)'] = ni_data[peaks]
+# plt.scatter(df['amplitude(v)'], df['hwidth(len)'])
+# plt.show()
+
 df.to_csv('data\\data.csv', index=False)
 df.to_csv(f'{fileName}_g{gate}-data.csv')
