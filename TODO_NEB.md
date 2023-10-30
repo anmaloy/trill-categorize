e615be885d2cf7c07ac90f5e54dfe7b0ea5a8c91
nbush <nbush257@gmail.com>
2023-10-30 11:07:10 -0700

    Add a visualize module and a todo file

---
+++ TODO_NEB.md
@@ -0,0 +1,22 @@
+# TODO list for NEB
+This is a working todo list for Nick to develop a maintainable code base for the xenopus vocalization project with Ayako Yamaguchi and Andrew Maloy.
+
+Will this link be permanent?:\
+https://drive.google.com/drive/u/1/folders/1P9llPoWhD0sqHmbLtnnXDDVb0jfx3hCK
+
+## TODO:
+
+- Move over a lot of utility functions from my older NPX code. 
+- Move over and comment the visualization functions from my notebooks
+- Reorganize modules, scripts, notebooks.
+
+## Optional:
+- Migrate to IBL convertions (ONE, alf) and code base?
+    - **Pros**
+        - Code base will extend with the IBL.
+        - Will likely be maintainable. 
+        - Less likely to have bugs for common operations
+    - **Cons** 
+        - Startup cost of reorganizing the code base and data.
+        - Have to stay up to date 
+        - Installation can be tricky
---
+++ visualize.py
@@ -0,0 +1,30 @@
+def map_event_to_time(events_df,t_exog,event_types=None):    
+    """Maps a dataframe of events to individual samples in time.
+    Given an events dataframe and a time vector, return a vector
+    of length len(t_exog) that labels each sample with the given event.
+
+    Args:
+        events_df (pandas dataframe): Data frame must have columns "start","end", and "type". Units must be seconds. 
+        t_exog (1D numpy vector): Must be an ordered time vector in seconds. 
+        event_types (list, optional): _description_. A list of event types to look for in the pandas dataframe. If None, it will map all unique events
+    
+    Returns:
+        t_label (1D numpy vector same length as t_exog, dtype is int)
+    """    
+    if event_types is None:
+        event_types = event_df['type'].unique()
+    t_label = np.zeros_like(t_exog,dtype='int')
+    for ii,evt in enumerate(event_types):
+        sub_events = events_df.query('type==@evt')
+        for start,stop in zip(sub_events['start'],sub_events['end']):
+            s0,sf = np.searchsorted(t_exog,[start,stop])
+            t_label[s0:sf]=ii+1
+    return(t_label)
+
+def shade_events(gate_events,ax,alpha=0.3):
+    event_types = ['I','F','S','V']
+    colors = ['r','g','b','y']
+    for ii,evt in enumerate(event_types):
+        sub_events = gate_events.query('type==@evt')
+        for start,stop in zip(sub_events['start'],sub_events['end']):
+            ax.axvspan(start,stop,color=colors[ii],alpha=alpha)
